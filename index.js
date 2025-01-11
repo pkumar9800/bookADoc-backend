3b2429a5b7f4aff30488e494774a784985934586
Pankaj Kumar <174971757+pkumar9800@users.noreply.github.com>
2025-01-11 22:04:39 +0530

    add database connection setup and user model

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express"
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from "./src/config/databaseConnection.js"
+
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+  res.send("API Working")
+});
+
+app.listen(port, () => console.log(`Server started on PORT:${port}`))
