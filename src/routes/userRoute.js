0529da26b7b8ff9250cc3296a4e5a448f9fc52fa
Pankaj Kumar <174971757+pkumar9800@users.noreply.github.com>
2025-02-04 23:12:08 +0530

    update user router

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,9 +1,24 @@
 import express from 'express';
-import { loginUser, registerUser} from '../controllers/userController.js';
+import { 
+  loginUser,
+  registerUser,
+  getProfile, 
+  updateProfile, 
+  bookAppointment, 
+  listAppointment, 
+  cancelAppointment, } from '../controllers/userController.js';
+import upload from '../middleware/multer.js';
+import authUser from '../middleware/authUser.js';
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser)
 userRouter.post("/login", loginUser)
+
+userRouter.get("/get-profile", authUser, getProfile)
+userRouter.post("/update-profile", upload.single('image'), authUser, updateProfile)
+userRouter.post("/book-appointment", authUser, bookAppointment)
+userRouter.get("/appointments", authUser, listAppointment)
+userRouter.post("/cancel-appointment", authUser, cancelAppointment)
 
 export default userRouter;
