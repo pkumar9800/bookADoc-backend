1be4517be658e6cd2408d66368417b70d9161b1f
Pankaj Kumar <174971757+pkumar9800@users.noreply.github.com>
2025-02-13 23:54:13 +0530

    update doctor controller

--- src/controllers/docterController.js
+++ src/controllers/docterController.js
@@ -87,9 +87,116 @@ const appointmentComplete = async (req, res) => {
 
 }
 
+// API to get all doctors list for Frontend
+const doctorList = async (req, res) => {
+    try {
+
+        const doctors = await doctorModel.find({}).select(['-password', '-email'])
+        res.json({ success: true, doctors })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+
+}
+
+// API to change doctor availablity for Admin and Doctor Panel
+const changeAvailablity = async (req, res) => {
+    try {
+
+        const { docId } = req.body
+
+        const docData = await doctorModel.findById(docId)
+        await doctorModel.findByIdAndUpdate(docId, { available: !docData.available })
+        res.json({ success: true, message: 'Availablity Changed' })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// API to get doctor profile for  Doctor Panel
+const doctorProfile = async (req, res) => {
+    try {
+
+        const { docId } = req.body
+        const profileData = await doctorModel.findById(docId).select('-password')
+
+        res.json({ success: true, profileData })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// API to update doctor profile data from  Doctor Panel
+const updateDoctorProfile = async (req, res) => {
+    try {
+
+        const { docId, fees, address, available } = req.body
+
+        await doctorModel.findByIdAndUpdate(docId, { fees, address, available })
+
+        res.json({ success: true, message: 'Profile Updated' })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// API to get dashboard data for doctor panel
+const doctorDashboard = async (req, res) => {
+    try {
+
+        const { docId } = req.body
+
+        const appointments = await appointmentModel.find({ docId })
+
+        let earnings = 0
+
+        appointments.map((item) => {
+            if (item.isCompleted || item.payment) {
+                earnings += item.amount
+            }
+        })
+
+        let patients = []
+
+        appointments.map((item) => {
+            if (!patients.includes(item.userId)) {
+                patients.push(item.userId)
+            }
+        })
+
+
+
+        const dashData = {
+            earnings,
+            appointments: appointments.length,
+            patients: patients.length,
+            latestAppointments: appointments.reverse()
+        }
+
+        res.json({ success: true, dashData })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
 export {
     loginDoctor,
     appointmentsDoctor,
     appointmentCancel,
-    appointmentComplete
+    appointmentComplete,
+    doctorList,
+    changeAvailablity,
+    doctorDashboard,
+    doctorProfile,
+    updateDoctorProfile
 }
