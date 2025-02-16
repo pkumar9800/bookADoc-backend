da1749855975342834e82d59d5b42881e552f69a
Pankaj Kumar <174971757+pkumar9800@users.noreply.github.com>
2025-02-16 22:34:16 +0530

    add doctor router

--- src/routes/docterRoute.js
+++ src/routes/docterRoute.js
@@ -1,5 +1,14 @@
 import express from 'express';
-import { loginDoctor, appointmentsDoctor, appointmentCancel, appointmentComplete } 
+import { 
+  loginDoctor, 
+  appointmentsDoctor, 
+  appointmentCancel, 
+  appointmentComplete,
+  doctorList, 
+  changeAvailablity,
+  doctorDashboard, 
+  doctorProfile, 
+  updateDoctorProfile } 
 from '../controllers/doctorController.js';
 import authDoctor from '../middleware/authDoctor.js';
 
@@ -9,6 +18,10 @@ doctorRouter.post("/login", loginDoctor)
 doctorRouter.post("/cancel-appointment", authDoctor, appointmentCancel)
 doctorRouter.get("/appointments", authDoctor, appointmentsDoctor)
 doctorRouter.post("/complete-appointment", authDoctor, appointmentComplete)
-
+doctorRouter.get("/list", doctorList)
+doctorRouter.post("/change-availability", authDoctor, changeAvailablity)
+doctorRouter.get("/dashboard", authDoctor, doctorDashboard)
+doctorRouter.get("/profile", authDoctor, doctorProfile)
+doctorRouter.post("/update-profile", authDoctor, updateDoctorProfile)
 
 export default doctorRouter;
