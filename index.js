1d29e19bcfece33f68e9ae74efa44ed04b81b10a
Pankaj Kumar <174971757+pkumar9800@users.noreply.github.com>
2025-02-25 22:42:57 +0530

    fix bugs

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from "./src/config/databaseConnection.js"
 import userRouter from "./routes/userRoute.js"
+import connectCloudinary from "./config/cloudinary.js"
+import doctorRouter from "./routes/doctorRoute.js"
+import adminRouter from "./routes/adminRoute.js"
 
 const app = express()
 const port = process.env.PORT || 4000
 connectDB()
+connectCloudinary()
 
 app.use(express.json())
 app.use(cors())
@@ -19,3 +23,5 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRouter)
+app.use("/api/admin", adminRouter)
+app.use("/api/doctor", doctorRouter)
