2d8a29530eb514104dc2f05f559dacdd1439d459
Pankaj Kumar <174971757+pkumar9800@users.noreply.github.com>
2025-02-28 23:41:29 +0530

    update admin controller

--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -1,6 +1,15 @@
 import express from 'express';
-import { loginAdmin, appointmentsAdmin, appointmentCancel } from '../controllers/adminController.js';
+import { 
+  loginAdmin, 
+  appointmentsAdmin, 
+  appointmentCancel,
+  addDoctor, 
+  allDoctors, 
+  adminDashboard
+ } from '../controllers/adminController.js';
 import authAdmin from '../middleware/authAdmin.js';
+import upload from '../middleware/multer.js';
+import { changeAvailablity } from '../controllers/doctorController.js';
 
 const adminRouter = express.Router();
 
@@ -8,5 +17,9 @@ const adminRouter = express.Router();
 adminRouter.post("/login", loginAdmin)
 adminRouter.get("/appointments", authAdmin, appointmentsAdmin)
 adminRouter.post("/cancel-appointment", authAdmin, appointmentCancel)
+adminRouter.post("/add-doctor", authAdmin, upload.single('image'), addDoctor)
+adminRouter.get("/all-doctors", authAdmin, allDoctors)
+adminRouter.post("/change-availability", authAdmin, changeAvailablity)
+adminRouter.get("/dashboard", authAdmin, adminDashboard)
 
 export default adminRouter;
